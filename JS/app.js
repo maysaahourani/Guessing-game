e063557c842f3aa8822d63edbaa0c0ab81a6b8e4
maysaahourani <maysaahourani@gmail.com>
2021-02-16 15:56:24 +0200

    maysaa

--- JS/app.js
+++ JS/app.js
@@ -2,49 +2,49 @@
 let username=prompt('what is yourname?');
 alert('WELCOME '+username+ '  to my gessing game throught these few questions try to answer with yes or no only');
 
-let movie=prompt.toLowerCase()(username +'do you think that i like movies ?');
+let movie=prompt(username +'do you think that i like movies ?').toLowerCase();
 console.log(movie);
 //movie.toLowerCase();
-if (movie===('yes'||'y'))
+if (movie==='yes'||movie==='y')
 {
   alert ('YES  ' + username+ ', I like movie specially drama movies') ;
 }
 
-if (movie===('no'||'n'))
+if (movie==='no'|| movie==='n')
 {
   alert ( 'thats wrong '+username +' , i love them :( ');
 }
 
 
 
-let stud=prompt.toLowerCase()('AM i a computer engineer ?');
+let stud=prompt('AM i a computer engineer ?').toLowerCase();
 console.log(stud);
 //stud.toLowerCase();
-if(stud===('yes'||'y'))
+if(stud==='yes'||stud==='y')
 {
   alert ('Great '+ username+' yes i am a computer engineer');
 }
-if(stud===('no'||'n'))
+if(stud==='no'||stud ==='n')
 {
   alert('unfortunatlly wrong answer,I am  the queen of computer engineering :P ');
 }
 
 
-let drink =prompt.toLowerCase()('NOW '+username+' do you think that i like coffee ?');
+let drink =prompt('NOW '+username+' do you think that i like coffee ?').toLowerCase();
 console.log(drink);
 //stud.toLowerCase();
-if(drink===('yes'||'y'))
+if(drink==='yes'|| drink==='y')
 {
   alert ('Great '+ username+' whos dont *.*');
 }
-if(drink===('no'||'n'))
+if(drink==='no'|| drink==='n')
 {
   alert('unfortunatlly wrong answer '+username+ 'you should repeat your calculations :P  ');
 }
 
 
 
-let live =prompt.toLowerCase()(username+' AM I from JORDAN ?');
+let live =prompt(username+' AM I from JORDAN ?').toLowerCase();
 console.log(drink);
 //stud.toLowerCase();
 if(live===('yes'||'y'))
@@ -57,7 +57,7 @@ if(live===('no'||'n'))
 }
 
 
-let age =prompt.toLowerCase()(username+' AM I older than 60 years old?');
+let age =prompt.toLowerCase()(username+' AM I older than 60 years old?').toLowerCase();
 console.log(age);
 //stud.toLowerCase();
 if(age===('yes'||'y'))
