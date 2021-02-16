a62a1d23a9b3c1d782de0fdb6559164ac3fac440
maysaahourani <maysaahourani@gmail.com>
2021-02-16 23:57:07 +0200

    maysaa

--- JS/app.js
+++ JS/app.js
@@ -68,20 +68,47 @@ if(age==='no'||age==='n')
 {
   alert('thats ture '+ username+ 'i hope i can reach this age in good health  ');
 }
-let ready=prompt('Now , we will play the guessing game number , are you ready ?').toLowerCase();
-console.log(ready);
+
+function numberGuess () {
+  let number = Math.floor(Math.random() * 100 + 1); // Generates a random number between 0 and 100
+
+  let totalGuesses = []; // Array to stores all the guesses
+  let guess = Number(prompt('Welcome to number guess!  Guess a number between 1 and 100')); // Prompts guessor to guess first number
+  while (guess !== number && totalGuesses.length<=4) { //loop runs while user has not guessed the number
+
+    if (!guess) { // User cancels prompts
+      return;
+    }
+    else if (totalGuesses.indexOf(guess) >= 0) { // Checks to see if guess has already been guessed
+      guess = prompt ('You already guessed that number before.  Give me another guess.');
+    }
+    else {
+      totalGuesses.push(guess);
+      if (guess > number) {
+        guess = prompt ('Your number is too high.  Try Again.  Dont give up!');
+      }
+
+      else {
+        guess = prompt ('Your number is too low.  Try Again.  I know you can do it!');
+      }
+    }
+  }
+  // Guess is corr
+
+  alert('Congratulations!  You guessed the number!  It took you  '+ (totalGuesses.length +1) + ' attempts!');
+}
+
+numberGuess();
+/*let numb=prompt('Now , we will play the guessing game number , are you ready choose a number?').toLowerCase();
+console.log(numb);
 
 //let array
 let counting=0;
-let numb=0;
-for(let i=0 ; counting<=4 ; i++)
+let guess=0;
+while(guess==='false')
 {
-  if (ready==='yes'|| ready==='y'){
-    numb=prompt ('As I mentioned '+username+' I want you to play guessing game number, so choose a number');
-    console.log(numb);
-  }
-
   if (numb === 15){
+
     alert('GREAT! '+username+'its the one :D');
     counting++;
   }
@@ -99,3 +126,4 @@ for(let i=0 ; counting<=4 ; i++)
     counting++;
   }
 }
+*/
