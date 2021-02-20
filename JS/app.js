234cf4a0641f1b33a6211078bc781b6bdd373c22
maysaahourani <maysaahourani@gmail.com>
2021-02-20 19:14:09 +0200

    CSS extra

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 let scoure=0;
 function aboutMe() {
 
@@ -9,7 +7,6 @@ function aboutMe() {
 
   let movie = prompt(username + 'do you think that i like movies ?').toLowerCase();
   console.log(movie);
-  //movie.toLowerCase();
   if (movie === 'yes' || movie === 'y') {
     alert('YES  ' + username + ', I like movie specially drama movies');
     scoure++;
@@ -18,12 +15,8 @@ function aboutMe() {
   if (movie === 'no' || movie === 'n') {
     alert('thats wrong ' + username + ' , i love them :( ');
   }
-
-
-
   let stud = prompt('AM i a computer engineer ?').toLowerCase();
   console.log(stud);
-  //stud.toLowerCase();
   if (stud === 'yes' || stud === 'y') {
     alert('Great ' + username + ' yes i am a computer engineer');
     scoure++;
@@ -31,11 +24,8 @@ function aboutMe() {
   if (stud === 'no' || stud === 'n') {
     alert('unfortunatlly wrong answer,I am  the queen of computer engineering :P ');
   }
-
-
   let drink = prompt('NOW ' + username + ' do you think that i like coffee ?').toLowerCase();
   console.log(drink);
-  //stud.toLowerCase();
   if (drink === 'yes' || drink === 'y') {
     alert('Great ' + username + ' whos dont *.*');
     scoure++;
@@ -43,12 +33,8 @@ function aboutMe() {
   if (drink === 'no' || drink === 'n') {
     alert('unfortunatlly wrong answer ' + username + 'you should repeat your calculations :P  ');
   }
-
-
-
   let live = prompt(username + ' AM I from JORDAN ?').toLowerCase();
   console.log(drink);
-  //stud.toLowerCase();
   if (live === 'yes' || live === 'y') {
     alert('YES thats sure ' + username + ' JORDAN FIRST');
     scoure++;
@@ -56,11 +42,9 @@ function aboutMe() {
   if (live === 'no' || live === 'n') {
     alert('O.o ' + username);
   }
-
-
   let age = prompt(username + ' AM I older than 60 years old?').toLowerCase();
   console.log(age);
-  //stud.toLowerCase();
+
   if (age === 'yes' || age === 'y') {
     alert('WHAT THE HELL' + username + ' go and drink your coffee');
   }
@@ -70,17 +54,6 @@ function aboutMe() {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
 function numberGuess() {
   let number = Math.floor(Math.random() * 10 + 1);
 
@@ -109,14 +82,6 @@ function numberGuess() {
   }
 
 }
-
-
-
-
-
-
-
-
 function guessMyFavColor() {
 
   let answerFavColor = ['red', 'green', 'blue', 'black'];
@@ -138,9 +103,6 @@ function guessMyFavColor() {
   }
 
 }
-
-
-
 aboutMe();
 numberGuess();
 guessMyFavColor();
